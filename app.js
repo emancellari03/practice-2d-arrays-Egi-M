e428a0a0fba6c2a2de1a38bdd7657cc274b2b531
Egi Mancellari <mancellariegi@gmail.com>
2026-05-03 22:51:50 -0500

    finished 2d arrays

---
+++ app.js
@@ -0,0 +1,22 @@
+let allScores = [
+  [84, 90, 88],
+  [78, 81, 80],
+  [97, 95, 77]
+];
+
+console.log(allScores);
+
+let theaterSeating = [
+  ["A1", "A2", "A3", "A4"],
+  ["B1", "B2", "B3", "B4"],
+  ["C1", "C2", "C3", "C4"]
+];
+
+console.log(theaterSeating);
+
+let contactsList = [
+  ["Hope Jones", "hope@email.com", "129-657"],
+  ["Mark Smith", "mark@email.com", "876-354"]
+];
+
+console.log(contactsList);
